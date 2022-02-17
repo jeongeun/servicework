bc1cb4101de565707da7a27cf302d3ea847f1536
Jeongeun Lee <jeongeun.lee@cern.ch>
2022-02-17 12:46:10 +0100

    fix problematic printing step

--- README.md
+++ README.md
@@ -1,21 +1,47 @@
-Steps
+*Aims :*
+Cleanup redundant cases of edm::ParameterSet calls to existAs or exist for tracked parameters, where redundancy is based on the value being already defined by fillDescriptions.
 
-(1) Get static_analyzer.log from the static analyzer
+*Motivation :*
+values of tracked parameters should be properly defined in the configuration and be visible in the process configuration provenance; code that checks for existence of a parameter and sets a hardcoded default would bypass the configuration provenance registration and should be avoided. In general, default parameter values should be provided via fillDescriptions, as detailed in SWGuideConfigurationValidationAndHelp. Redundant calls to existAs or exist can be cleaned up.
 
-(2) Running mapping.sh reco-package name 
 
-> ./mapping.sh RecoMuon
+The list of all calls of existAs or exist are available in the Static Analyzer reports accessible e.g. from IB page. 
+For this task consider only modules or tools used by the modules that define fillDescriptions. 
+The following cases can be considered redundant in this set and straightforward for cleanup:
 
-**(a)** Read static_analyzer.log and make a list of files only in the RecoMuon
+- There is a setDefaults with a value the same as the hardcoded default in the existAs or exist logic.
+- The fillDescriptions defines a configuration in cfipython/ with this parameter and instances of this module type already use the cfipython/ (we assume here also that the HLT configurations for these cases already define the parameter explicitly; as typically is the rule for HLT configurations)
+
+
+*reference twiki :*
+https://twiki.cern.ch/twiki/bin/viewauth/CMS/OpenRecoTasks
+https://cmssdt.cern.ch/SDT/html/cmssdt-ib/#/ib/CMSSW_12_3_X
+https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideConfigurationValidationAndHelp
+
+
+*Steps :*
+
+(1) Get reco_Bug.log from the Static Analyzer reports (from IB page 12_3_X for now)
+
+(2) Running mapping.sh <reco-package name> 
+
+-- for example :
+
+> ./mapping.sh RecoMuon > recomuon.log
+
+-- workflow in mapping.sh :
+**(a)** Read reco_Bug.log and make a list of files only in the RecoMuon
 
 **(b)** Print out following information
 
    * Target Source file (.cc)
-   * EDModule type name (function)
-   * Line with exist or existAs
-   * parameter name
+   * ED module name (function)
+   * The line where the fillDescription is located in the source file
+   * cfipython file containg the same/similar module
+   * The line where the exist or existAs are located in the source file
+   * The parameter name
 
-**(c)** Check whether the parameter defined with fillDescription in the source => YES or No
-**(d)** Check if there is cfipython mapping hint (duplicated case) => YES or No
+**(c)** Check whether the parameter also defined with fillDescription in the source file => if yes 1, no 0. 
+**(d)** Check if the parameter is in the cfipython (referring to the slava's mapping hint script) => if yes 1, no 0.
 
-(3) If any (c) or (d) is "YES", then print message "====> !!!! Update !!!!"
+(3) If any (c) or (d) is set to 1, then print message "Please check & update !"
--- mapping.sh
+++ mapping.sh
@@ -6,7 +6,7 @@ if [ "$RECOSET" == "" ]; then
       break;
 fi
 
-cat reco_Bug.log | grep $RECOSET | awk '{print $0}' | sort -u >> $RECOSET.txt
+cat reco_Bug.log | grep $RECOSET | awk '{print $1 "	" $3}' | sort -u >> $RECOSET.txt
 INPUT=$RECOSET.txt
 echo "************$RECOSET****************";
 
@@ -15,73 +15,85 @@ do
   echo "***********************************************************************";
 
   SOURCE=`echo $line | awk '{print $1}'`               # target source file
-  FUNC=`echo $line | awk '{print $3}'`                 # function/method
+  FUNC=`echo $line | awk '{print $2}'`                 # function/method
   EXIST=`grep -r -n "exist" ./$SOURCE`                 # line with exist or existAs
 
-  echo "#TargetCC: $SOURCE     #Function: $FUNC";      # print out
-  echo "$EXIST";
+  echo "#targetCC: $SOURCE     #function: $FUNC";      # print out
 
   ### FillDesctiprion Check ###
 
   FILLDESC_CHECK1=0
   FILLDESC_CHECK2=0
   FILLDESC_Num=0
-  FILLDESC=`grep -r -n "::fillDescriptions(edm::" ./$SOURCE` # check fillDescription used in this source file
+  FILLDESC=`grep -r -n "::fillDescriptions(edm::ConfigurationDescription" ./$SOURCE` # check fillDescription used in this source file
+  #echo "filldescription = $FILLDESC"
 
   if [ "$FILLDESC" != ""  ];then
      FILLDESC_CHECK1=1                                       # if fillDescription is used in the source, then set to 1
      FILLDESC_Num=`echo $FILLDESC | awk -F':' '{print $1}'`  # keep the line number (fillDescription)
-     echo "###FillDescription : $FILLDESC"                   # print out
+     echo "#fillDescription: $FILLDESC"                     # print out
   fi
 
   ### cfipython Check ###
 
   CFI_CHECK1=0
   CFI_CHECK2=0
+  CFIPYTHON=""
+  CFI=""
+  CFI_PARAM=""
   CFIPYTHON=`grep -r cms.ED $CMSSW_RELEASE_BASE/cfipython/$SCRAM_ARCH/[A-Z]* | cut -d"/" -f10- | sed -e "s/[':=(,]/ /g;s/[ ]* / /g;s/ $//" | grep ${FUNC}`  # find relavant cfipython
-  if [ "$CFIPYTHON" != ""  ];then
-     CFI_CHECK1=1                                                       # if there is a relevant cfipython, then set to 1
-     CFI=`echo ../cfipython/$SCRAM_ARCH/$CFIPYTHON | awk '{print $1}'`  # only cfipython file directory and name
-     echo "###Cfipython : $CFIPYTHON";                                  # print out
+  if [ "$CFIPYTHON" != ""  ];then                                      # if there is a relevant cfipython, then set to 1
+     CFI_CHECK1=1
+     CFI=`echo ../cfipython/$SCRAM_ARCH/$CFIPYTHON | awk '{print $1}'` # only cfipython file directory and name
+     #echo "# cfipython: $CFIPYTHON";                                  # print out
+     echo "#cfipython: $CFI";                                               # print out
   fi
+  #echo "cfipython = $CFIPYHION  cfi = $CFI"
+  echo "$EXIST";
 
   ### parameter loop ###
 
   while IFS= read -r EXIST                    # loop for check all parameters call to exis one by one
   do
-        PARAM=`echo $EXIST | cut -d '"' -f2`  # check parameter name
+      PARAM=`echo $EXIST | cut -d '"' -f2`    # check parameter name
+      if [ "${PARAM}" != "" ];then
+            CC_PARAM=""
+            CC_PARAM=`grep -r -n $PARAM ./$SOURCE | grep -Ev'exist'`  # check other lines using this parameter in the source
+            CC_PARAM_Num=`echo $CC_PARAM | awk -F':' '{print $1}'`    # keep the line number ( parameter)
+            echo "###pramCC: $PARAM  inCC: $CC_PARAM_Num  inFillDesc: $FILLDESC_Num " ;
+            echo "$CC_PARAM" ;                              # print out
 
-        if [ "${PARAM}" != "" ];then
-              CC_PARAM=""
-              CC_PARAM=`grep -r -n $PARAM ./$SOURCE | grep -Ev'exist'`  # check other lines using this parameter in the source
-              CC_PARAM_Num=`echo $CC_PARAM | awk -F':' '{print $1}'`    # keep the line number ( parameter)
-              echo "###pramCC: $PARAM  lineCC: $CC_PARAM_Num lineFill: $FILLDESC_Num" ;     # print out
+            if [ "${FILLDESC_Num}" != "0"  -a "${CC_PARAM_Num}" != "" -a ${CC_PARAM_Num} -gt ${FILLDESC_Num} ];then 
+                 FILLDESC_CHECK2=1                                    # if the parameter is also defined after fillDescription line, then set to 1
+                 echo "--- in filldesc cc : $CC_PARAM";                        # print the reducdant line (parameter) in the source file
+            else
+                 echo "--- not found in filldesc cc"
+            fi
 
-              if [ "${FILLDESC_Num}" != "0"  -a "${CC_PARAM_Num}" != "" -a ${CC_PARAM_Num} -gt ${FILLDESC_Num} ];then 
-                   FILLDESC_CHECK2=1                                    # if the parameter is also defined after fillDescription line, then set to 1
-                   echo "--- In cc : $CC_PARAM";                            # print the reducdant line (parameter) in the source file
-              fi
-
-              if [ "{$CFI_CHECK1}" != "0" ]; then                                          
-                    CFI_PARAM=`grep -r -n $PARAM $CFI`                  # check all other lines using this parameter in the cfipython
-                    if [ "$CFI_PARAM" != "" ];then                  
-                         CFI_CHECK2=1                                   # if the parameter is also defined in cfipython file, then set to 1 
-                         echo "--- In cfipy : $CFI_PARAM";                  # print the redundant line in the cfipython file
-                    fi
-              fi
-        fi
+            if [ "{$CFI_CHECK1}" != "0" -a "${CFI}"  ]; then                                          
+                  CFI_PARAM=`grep -r -n $PARAM $CFI`                  # check all other lines using this parameter in the cfipython
+                  if [ "$CFI_PARAM" != "" ];then                  
+                       CFI_CHECK2=1                                   # if the parameter is also defined in cfipython file, then set to 1 
+                       echo "--- in cfipy : $CFI_PARAM";                  # print the redundant line in the cfipython file
+                  else
+                       echo "--- not found in cfi"
+                  fi
+            fi
+            
+      fi
 
   done <<< "$(grep -r "exist" ./$SOURCE)" # loop finished
 
   ### Final check ###
 
-  echo "### FinalCheck ccfillDesc $FILLDESC_CHECK1 $FILLDESC_CHECK2  cfipython $CFI_CHECK1 $CFI_CHECK2";
+  echo "###FinalCheck fillDesc $FILLDESC_CHECK1 redundent $FILLDESC_CHECK2  cfipython $CFI_CHECK1 redeudent $CFI_CHECK2";
 
   if [ $FILLDESC_CHECK1 -eq 1 -o $FILLDESC_CHECK2 -eq 1 -o  $CFI_CHECK1 -eq 1 -o $CFI_CHECK2 -eq 1 ];then
-        echo "==> Please Check and Update $SOURCE !!!  $FILLDESC_CHECK1 $FILLDESC_CHECK2 $CFI_CHECK1 $CFI_CHECK2 $CFI";
-        echo "###################################################################3";
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+        echo "Please check $SOURCE ! $FILLDESC_CHECK1 $FILLDESC_CHECK2 $CFI_CHECK1 $CFI_CHECK2 $CFI";
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
   fi
 
 done < "$INPUT"
 
-
+rm -rf $INPUT
